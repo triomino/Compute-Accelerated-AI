c44bd5b2b8f0c34aca81143d579b6560b8bccf34
Zhang Yuan <zjuzhangyuan@gmail.com>
2026-04-13 14:33:12 +0800

    Implement ForkedPdb for multi-process debugging
    Added ForkedPdb class for debugging multi-processed code, allowing interaction with the debugger in subprocesses.

---
+++ script/forked_pdb.py
@@ -0,0 +1,16 @@
+import pdb
+import sys
+from typing import Any
+
+class ForkedPdb(pdb.Pdb):
+    """
+    PDB Subclass for debugging multi-processed code
+    Suggested in: https://stackoverflow.com/questions/4716533/how-to-attach-debugger-to-a-python-subproccess
+    """
+    def interaction(self, *args: Any, **kwargs: Any) -> None:
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open("/dev/stdin")
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
